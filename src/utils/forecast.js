b6286aae5308d888367b566a1968a26ed4ac3451
Stefana Cotos <timofte.stefana@gmail.com>
2020-05-04 13:58:39 +0300

    Added humidity percentage in the weather data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,13 +4,13 @@ const forecast = (latitude, longitude, callback) => {
   const url = 'http://api.weatherstack.com/current?access_key=d71137ede6c669b885b63681ece3a0c9&query='+ encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) + '&units=m'
 
   request({url, json: true}, (error, {body}= {}) => {
-    const {weather_descriptions, temperature, feelslike} = body.current
+    const {weather_descriptions, temperature, feelslike, humidity} = body.current
     if(error) {
       callback('Unable to connect to the weather service!', undefined)
     } else if (body.error) {
       callback('Unable to find location!', undefined)
     } else {
-      callback(undefined, weather_descriptions[0] + '. It is currently ' + temperature + ' degrees out. It feels like ' + feelslike + ' degress out.')
+      callback(undefined, weather_descriptions[0] + '. It is currently ' + temperature + ' degrees out. It feels like ' + feelslike + ' degress out. And the humidity is ' + humidity + '%.')
     }
   })
 }
